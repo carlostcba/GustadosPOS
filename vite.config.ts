5b839cdd61d466279804dfaf5124892741384a62
carlostcba <33902391+carlostcba@users.noreply.github.com>
2025-04-10 15:42:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
-    host: '0.0.0.0', // Escucha en todas las interfaces de red
-    port: 5173      // Puedes cambiar el puerto si lo necesitas
-  }
+    host: '0.0.0.0',
+    port: 5173,
+    origin: 'https://api.lasalleflorida.edu.ar',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
 });
